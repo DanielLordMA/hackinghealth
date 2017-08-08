b4025f230af2d5ba07e1cd6ced091f3ecdf097f7
DanielLordMA <daniell@medicalert.co.nz>
2017-08-08 15:15:35 +1200

    visual studio initial commit

---
+++ vssource/hh2017/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace hh2017
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/glo").Include(
+                        "~/Scripts/global-min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/style.css"));
+        }
+    }
+}
---
+++ vssource/hh2017/Content/Controllers/demoController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Helpers;
+
+namespace hh2017.Controllers
+{
+    public class demoController : Controller
+    {
+        // GET: demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Member(string memberId)
+        {
+
+            ViewBag.MemberId = memberId;
+
+            ViewBag.JsonData = System.Web.Helpers.Json.Decode(GET("http://test.healthintersections.com.au/medicalert/Patient/196209/$everything"));
+            ViewBag.Member = ((IEnumerable<dynamic>)ViewBag.JsonData.entry).Where(d => d.resource.resourceType == "Patient").FirstOrDefault().resource;
+
+            return View();
+        }
+
+
+        string GET(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Accept = "application/json+fhir";
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    // log errorText
+                }
+                throw;
+            }
+        }
+
+
+
+
+
+    }
+}
