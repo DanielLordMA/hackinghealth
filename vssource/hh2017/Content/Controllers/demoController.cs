ff59e1d32d8a36ec6789793a2f2eea61dc00366e
DanielLordMA <daniell@medicalert.co.nz>
2017-08-08 19:24:00 +1200

    copy updates from root

--- vssource/hh2017/Content/Controllers/demoController.cs
+++ vssource/hh2017/Content/Controllers/demoController.cs
@@ -13,8 +13,9 @@ namespace hh2017.Controllers
     public class demoController : Controller
     {
         // GET: demo
-        public ActionResult Index()
+        public ActionResult Index(string error = "")
         {
+            ViewBag.error = error;
             return View();
         }
 
@@ -25,19 +26,24 @@ namespace hh2017.Controllers
             ViewBag.MemberId = memberId;
             //196209
 
-            ViewBag.JsonData = System.Web.Helpers.Json.Decode(GET("http://test.healthintersections.com.au/medicalert/Patient/" + memberId + "/$everything"));
+            ViewBag.Result = GET("http://test.healthintersections.com.au/medicalert/Patient/" + memberId + "/$everything");
+            if (((string)ViewBag.Result).StartsWith("Error")){
+                return RedirectToAction("Index",new{ error = (string)ViewBag.Result});
+            }
+
+            ViewBag.JsonData = System.Web.Helpers.Json.Decode(ViewBag.Result);
 
             ViewBag.Member = ((IEnumerable<dynamic>)ViewBag.JsonData.entry).Where(d => d.resource.resourceType == "Patient").DefaultIfEmpty(new { resource = new { } }).Select(d => d.resource).First();
             ViewBag.Conditions = ((IEnumerable<dynamic>)ViewBag.JsonData.entry).Where(d => d.resource.resourceType == "Condition").Select(d => d.resource);
-            ViewBag.AllergyIntolerance = ((IEnumerable<dynamic>)ViewBag.JsonData.entry).Where(d => d.resource.resourceType == "AllergyIntolerance").Select(d => d.resource); 
+            ViewBag.AllergyIntolerance = ((IEnumerable<dynamic>)ViewBag.JsonData.entry).Where(d => d.resource.resourceType == "AllergyIntolerance").Select(d => d.resource);
             ViewBag.MedicationStatement = ((IEnumerable<dynamic>)ViewBag.JsonData.entry).Where(d => d.resource.resourceType == "MedicationStatement").Select(d => d.resource);
 
             ViewBag.MemberId = Identifier(ViewBag.Member.Identifier, "http://medicalert.nz/member-id");
             ViewBag.MemberNHI = Identifier(ViewBag.Member.Identifier, "http://health.govt.nz/nhi");
             ViewBag.MemberIHI = Identifier(ViewBag.Member.Identifier, "http://health.govt.au/IHI");
 
-            ViewBag.BloodGroup = ((IEnumerable<dynamic>)ViewBag.JsonData.entry).Where(d => d.resource.resourceType == "Observation").Select(d => d.resource.code.coding.display);
-                //.First() ?? "unknown";
+            //ViewBag.BloodGroup = ((IEnumerable<dynamic>)ViewBag.JsonData.entry).Where(d => d.resource.resourceType == "Observation").Select(d => d.resource.code.coding.display);
+            //.First() ?? "unknown";
             //&& d.resource.code.codeing.system == "http://loinc.org"
 
             return View();
@@ -67,15 +73,17 @@ namespace hh2017.Controllers
                     String errorText = reader.ReadToEnd();
                     // log errorText
                 }
-                throw;
+                return "Error - no member found with That ID";
             }
         }
 
-        dynamic Entry(dynamic data,string entryName) { 
-            return ((IEnumerable<dynamic>)data.entry).Where(d => d.resource.resourceType == entryName).DefaultIfEmpty(new { resource = new { } }).Select(d=> d.resource);
+        dynamic Entry(dynamic data, string entryName)
+        {
+            return ((IEnumerable<dynamic>)data.entry).Where(d => d.resource.resourceType == entryName).DefaultIfEmpty(new { resource = new { } }).Select(d => d.resource);
         }
 
-        string Identifier(dynamic data,string system) { 
+        string Identifier(dynamic data, string system)
+        {
             return ((IEnumerable<dynamic>)data).Where(d => d.system == system).DefaultIfEmpty(new { value = "" }).FirstOrDefault().value ?? "";
         }
 
